b5aa3fbc98a02a3c60192973690b7d98b743c950
MateusLima04 <mateuslimaa04@gmail.com>
2026-05-21 22:28:33 -0300

    Interface

--- pokemon/src/app/home/home.page.ts
+++ pokemon/src/app/home/home.page.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonCardHeader, IonCard, IonCardContent, IonCardTitle } from '@ionic/angular/standalone';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/angular/standalone';
+import { PokemonService } from '../services/pokemon.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonCardHeader, IonCard, IonCardContent, IonCardTitle],
+  imports: [CommonModule, IonHeader, IonToolbar, IonTitle, IonContent, IonButton],
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit {
+  pokemonEsquerda: any = null;
+  pokemonDireita: any = null;
+  carregando = false;
+
+  constructor(private pokemonService: PokemonService) {}
+
+  ngOnInit() {
+    this.gerarBatalha();
+  }
+
+  gerarBatalha() {
+    this.carregando = true;
+    
+    this.pokemonService.getPokemonAleatorio().subscribe(
+      (pokemon1) => {
+        this.pokemonEsquerda = pokemon1;
+        
+        this.pokemonService.getPokemonAleatorio().subscribe(
+          (pokemon2) => {
+            this.pokemonDireita = pokemon2;
+            this.carregando = false;
+          },
+          (erro) => {
+            console.error('Erro ao buscar Pokémon direita:', erro);
+            this.carregando = false;
+          }
+        );
+      },
+      (erro) => {
+        console.error('Erro ao buscar Pokémon esquerda:', erro);
+        this.carregando = false;
+      }
+    );
+  }
 }
---
+++ pokemon/src/app/services/pokemon.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokemonService {
+  private apiUrl = 'https://pokeapi.co/api/v2';
+
+  constructor(private http: HttpClient) {}
+
+  getPokemonAleatorio(): Observable<any> {
+    const id = Math.floor(Math.random() * 151) + 1; // Pokémon de 1 a 151
+    return this.http.get(`${this.apiUrl}/pokemon/${id}`);
+  }
+}
